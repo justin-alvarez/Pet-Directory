6a34c786a8d0fa95f9c2e1035ff8b1de29ce91bf
Justin <justinalvarez.gg@gmail.com>
2022-10-26 18:28:48 -0700

    Add website contents

---
+++ js/pets.js
@@ -0,0 +1,51 @@
+const pets = [
+{name:'Ace',
+ type: 'Dog',
+ breed: 'Australian Shepherd',
+ age: 5,
+ photo: 'img/aussie.jpg', 
+ alt: 'Australian Shepard'
+},
+
+{name: 'Amber',
+ type: 'Dog',
+ breed: 'Dachshund',
+ age: 2,
+ photo: 'img/dachshund.jpg', 
+ alt: 'Dachshund'
+},
+
+{name:'Rover',
+ type: 'Dog',
+ breed: 'Golden Retriever',
+ age: 4,
+ photo: 'img/golden.jpg',
+ alt: 'Golden Retriever'
+},
+
+{name:'Max',
+ type: 'Cat',
+ breed: 'Persian Cat',
+ age: 8,
+ photo: 'img/persian.jpg', 
+ alt: 'Persian Cat'
+},
+
+{name:'Peanuts',
+ type: 'Dog',
+ breed: 'Pug',
+ age: 1,
+ photo: 'img/pug.jpg', 
+ alt:'Persian Cat'
+},
+
+{name:'Kobe',
+ type: 'Cat',
+ breed: 'Tabby American shorthair',
+ age: 2,
+ photo:'img/tabby.jpg', 
+ alt: 'Tabby American shorthair'
+}
+]
+
+
